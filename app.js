e8a2e8e61fba8decb9e3a79b3a036c17592c0724
tianwen-chen <tianwen.chen.25@dartmouth.edu>
2023-10-22 12:56:59 -0400

    add all files

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { create, result } = require('lodash');
+const { default: mongoose } = require('mongoose');
+const morgan = require('morgan');
+const Blog = require('./models/blogs');
+const { urlencoded } = require('body-parser');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect mongoDB & listen for requests
+const NAME = process.env.USER_NAME;
+const PASSWORD = process.env.USER_PASSWORD;
+const CLUSTER_NAME = process.env.CLUSTER_NAME;
+const dbURI = `mongodb+srv://${NAME}:${PASSWORD}@${CLUSTER_NAME}.mongodb.net/`
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( (result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title : 'About'});
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', {title: '404'});
+});
